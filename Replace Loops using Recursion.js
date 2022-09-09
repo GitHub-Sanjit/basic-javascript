298452a05fd2d180e56c033aeaac6250a26a838d
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-09-09 09:44:10 +0600

    Replace Loops Using Recursion

--- Replace Loops using Recursion.js
+++ Replace Loops using Recursion.js
@@ -1,5 +1,27 @@
+// Loops
+  function multiply(arr, n) {
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+    return product;
+  }
+
+
 // Replace Loops using Recursion
 
+// 1.
+  function multiply(arr, n) {
+    if (n <= 0) {
+      return 1;
+    } else {
+      return multiply(arr, n - 1) * arr[n - 1];
+    }
+  }
+
+
+  
+//   2.
 function sum(arr, n) {
   if(n <= 0) {
     return 0;
